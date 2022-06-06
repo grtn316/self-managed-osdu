0aac2b1303274db1cbcbde964f4920cc3e0a3be5
Corey Callaway <coreylcallaway@gmail.com>
2022-06-06 12:22:40 -0400

    Update network-existing.sh

--- scripts/network-existing.sh
+++ scripts/network-existing.sh
@@ -3,6 +3,8 @@
 # If NETWORK_EXISTING is set to true, copy main.tf and diagnostics.tf from /osdu-azure/templates/osdu-r3-mvp/network_existing/ to /osdu-azure/templates/osdu-r3-mvp/service_resources/
 # Else if NETWORK_EXISTING is set to false, echo "NETWORK_EXISTING is set to false, creating new network reousrces"
 
+NETWORK_EXISTING=${1:-"false"}
+
 if [ "${NETWORK_EXISTING}" = "true" ]; then
     echo "NETWORK_EXISTING is set to true, copying main.tf and diagnostics.tf from /osdu-azure/templates/osdu-r3-mvp/network_existing/ to /osdu-azure/templates/osdu-r3-mvp/service_resources/"
     cp /osdu-azure/templates/osdu-r3-mvp/network_existing/main.tf /osdu-azure/templates/osdu-r3-mvp/service_resources/
